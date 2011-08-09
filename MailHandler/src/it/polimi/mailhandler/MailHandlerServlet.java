482458f9cd6eeb72b9b3df99c28fbec6cc861adb
andrea.colombo88@gmail.com <andrea.colombo88@gmail.com@998307cd-94c5-57b4-f45a-eb15fd97a752>
2011-08-09 13:58:57 +0000

    Spaccca i culi.

--- MailHandler/src/it/polimi/mailhandler/MailHandlerServlet.java
+++ MailHandler/src/it/polimi/mailhandler/MailHandlerServlet.java
@@ -16,8 +16,12 @@ public class MailHandlerServlet extends HttpServlet {
 import java.io.IOException; 
 import java.util.Properties; 
 
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session; 
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage; 
 import javax.servlet.http.*; 
 
@@ -33,9 +37,25 @@ public class MailHandlerServlet extends HttpServlet {
             throws IOException { 
         Properties props = new Properties(); 
         Session session = Session.getDefaultInstance(props, null); 
+        String msgBody = "Messaggio ricevuto.";
         try {
 			MimeMessage message = new MimeMessage(session, req.getInputStream());
-			System.out.print(message.getFrom()[0].toString());
+			String mittente=message.getFrom()[0].toString();
+	        try {
+	            Message msg = new MimeMessage(session);
+	            msg.setFrom(new InternetAddress("reply@polimigcourse.appspotmail.com", "PolimiGCourse"));
+	            System.out.println(mittente);
+	            msg.addRecipient(Message.RecipientType.TO,message.getFrom()[0]);
+	            //msg.addRecipient(Message.RecipientType.TO, new InternetAddress("andrea.colombo88@gmail.com", "Mr. User"));
+	            msg.setSubject("Your Example.com account has been activated");
+	            msg.setText(msgBody);
+	            Transport.send(msg);
+
+	        } catch (AddressException e) {
+	            // ...
+	        } catch (MessagingException e) {
+	            // ...
+	        }
 		} catch (MessagingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
